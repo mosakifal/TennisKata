0000171c9b651b9ac3ab766591217e8d5d7ebb40
mohamed adjagbe <mosakifal@hotmail.com>
2020-06-05 11:05:35 +0000

    Initial commit Added the unit test project

---
+++ TennisKata.Tests/TennisKata.Tests/TennisGameScoreTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace TennisKata.Tests
+{
+    public class TennisGameScoreTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Return_ScoreFifteenLove_WhenPlayerOne_WinAPoint()
+        {
+            Assert.Pass();
+        }
+    }
+}
